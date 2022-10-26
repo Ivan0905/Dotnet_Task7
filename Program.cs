d4e4f8893e812f34721c01ae1792c46a2a8db235
Фарафонов Иван Алексеевич <i.farafonov@ktpproekt.local>
2022-10-26 16:49:03 +0300

    Решена задача 50

--- Program.cs
+++ Program.cs
@@ -3,14 +3,69 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
-int rows = 3;
-int columns = 4;
-double[,] array = GetArray(rows, columns, -9.5, 9.5);
-PrintArr(array);
+// int rows = 3;
+// int columns = 4;
+// double[,] array = GetArray(rows, columns, -9.5, 9.5);
+// PrintArr(array);
 
-double[,] GetArray(int n, int m, double min, double max)
+// double[,] GetArray(int n, int m, double min, double max)
+// {
+//     double[,] arr = new double[n, m];
+
+//     for (int i = 0; i < n; i++)
+//     {
+
+//         for (int j = 0; j < m; j++)
+//         {
+
+//             arr[i, j] = new Random().NextDouble() * (max - min) + min;
+//         }
+//     }
+//     return arr;
+// }
+
+// void PrintArr(double[,] arr)
+// {
+
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             Console.Write(" {0:f2} ", arr[i, j]);
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// i = 4, j = 2 -> такого числа в массиве нет
+// i = 1, j = 2 -> 2
+
+Console.WriteLine("Введите номер строки");
+int row = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите номер столбца");
+int column = int.Parse(Console.ReadLine()!);
+int [,] array = GetArray(4,4,0,5);
+PrintArr(array);
+text(array,row,column);
+void text (int[,] arr, int r, int c){
+
+if(r>=0&&r<=arr.GetLength(0)-1&&c>=0&&c<=arr.GetLength(1)-1){
+    Console.WriteLine(arr[r,c]);
+}
+else{
+    Console.WriteLine("Такого элемента нет");
+}
+}
+
+
+int[,] GetArray(int n, int m, int min, int max)
 {
-    double[,] arr = new double[n, m];
+    int[,] arr = new int[n, m];
 
     for (int i = 0; i < n; i++)
     {
@@ -18,20 +73,20 @@ double[,] GetArray(int n, int m, double min, double max)
         for (int j = 0; j < m; j++)
         {
 
-            arr[i, j] = new Random().NextDouble() * (max - min) + min;
+            arr[i, j] = new Random().Next(min,max+1);
         }
     }
     return arr;
 }
 
-void PrintArr(double[,] arr)
+void PrintArr(int[,] arr)
 {
 
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
         {
-            Console.Write(" {0:f2} ", arr[i, j]);
+            Console.Write($"{arr[i, j]} ");
         }
         Console.WriteLine();
     }
